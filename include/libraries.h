cc1f094e44ff9d61b18d26997d1b9e4d17acbc86
3lsy <echavez-@student.42.fr>
2024-03-05 15:43:36 +0100

    base .h with required libraries

---
+++ include/libraries.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libraries.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 15:38:48 by echavez-          #+#    #+#             */
+/*   Updated: 2024/03/05 15:39:16 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBRARIES_H
+# define LIBRARIES_H
+
+# include "libft.h"
+# include <stdio.h>    // printf
+# include <stdlib.h>   // malloc, free, exit
+# include <fcntl.h>    // open file
+# include <unistd.h>   // close, read, write
+# include <errno.h>    // perror, strerror
+# include <math.h>     // math functions (-lm required during compilation)
+# include <mlx.h>      // MinilibX
+# include <mlx_int.h>  // MinilibX
+
+#endif
