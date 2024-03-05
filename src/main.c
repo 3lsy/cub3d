eed0524b3eeba899609c13d041d94ebce2db1449
3lsy <echavez-@student.42.fr>
2024-03-05 15:53:27 +0100

    norme

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:20:48 by echavez-          #+#    #+#             */
-/*   Updated: 2024/03/05 15:41:12 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:47:45 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-
 	printf("Hello Cubes of the World!/n");
 	return (0);
 }
