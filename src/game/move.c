b1417e5d2b141d31ad5916893a4469e0a15fed4e
3lsy <echavez-@student.42.fr>
2024-04-18 01:26:04 +0200

    collision

--- src/game/move.c
+++ src/game/move.c
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 17:32:16 by echavez-          #+#    #+#             */
-/*   Updated: 2024/03/10 20:22:51 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/04/18 01:24:55 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,86 @@
 //  world->player.mx, world->player.my);
 void	move_up(t_cub3d *world)
 {
-	world->player.mx += cos(world->player.angle) * world->player.move_speed;
-	world->player.my -= sin(world->player.angle)
-		* world->player.move_speed;
+	double	x;
+	double	y;
+
+	x = world->player.mx + cos(world->player.angle) * world->player.move_speed;
+	y = world->player.my - sin(world->player.angle) * world->player.move_speed;
+	if (world->graphics.bmp[(int)y][(int)x] == MMAP_WALL
+		|| world->graphics.bmp[(int)y][(int)x] == 0x000000
+		|| world->graphics.bmp[(int)y][(int)x] == FOV_COLOR
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == '1'
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == ' '
+		|| x < 0 || y < 0 || x > world->graphics.mmap_w
+		|| y > world->graphics.mmap_h)
+		return ;
+	world->player.mx = x;
+	world->player.my = y;
 	render(world);
 }
 
 void	move_down(t_cub3d *world)
 {
-	world->player.mx += cos(world->player.angle + M_PI)
+	double	x;
+	double	y;
+
+	x = world->player.mx + cos(world->player.angle + M_PI)
 		* world->player.move_speed;
-	world->player.my -= sin(world->player.angle + M_PI)
+	y = world->player.my - sin(world->player.angle + M_PI)
 		* world->player.move_speed;
+	if (world->graphics.bmp[(int)y][(int)x] == MMAP_WALL
+		|| world->graphics.bmp[(int)y][(int)x] == 0x000000
+		|| world->graphics.bmp[(int)y][(int)x] == FOV_COLOR
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == '1'
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == ' '
+		|| x < 0 || y < 0 || x > world->graphics.mmap_w
+		|| y > world->graphics.mmap_h)
+		return ;
+	world->player.mx = x;
+	world->player.my = y;
 	render(world);
 }
 
 void	move_left(t_cub3d *world)
 {
-	world->player.mx += cos(world->player.angle + M_PI / 2)
+	double	x;
+	double	y;
+
+	x = world->player.mx + cos(world->player.angle + M_PI / 2)
 		* world->player.move_speed;
-	world->player.my -= sin(world->player.angle + M_PI / 2)
+	y = world->player.my - sin(world->player.angle + M_PI / 2)
 		* world->player.move_speed;
+	if (world->graphics.bmp[(int)y][(int)x] == MMAP_WALL
+		|| world->graphics.bmp[(int)y][(int)x] == 0x000000
+		|| world->graphics.bmp[(int)y][(int)x] == FOV_COLOR
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == '1'
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == ' '
+		|| x < 0 || y < 0 || x > world->graphics.mmap_w
+		|| y > world->graphics.mmap_h)
+		return ;
+	world->player.mx = x;
+	world->player.my = y;
 	render(world);
 }
 
 void	move_right(t_cub3d *world)
 {
-	world->player.mx += cos(world->player.angle - M_PI / 2)
+	double	x;
+	double	y;
+
+	x = world->player.mx + cos(world->player.angle - M_PI / 2)
 		* world->player.move_speed;
-	world->player.my -= sin(world->player.angle - M_PI / 2)
+	y = world->player.my - sin(world->player.angle - M_PI / 2)
 		* world->player.move_speed;
+	if (world->graphics.bmp[(int)y][(int)x] == MMAP_WALL
+		|| world->graphics.bmp[(int)y][(int)x] == 0x000000
+		|| world->graphics.bmp[(int)y][(int)x] == FOV_COLOR
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == '1'
+		|| world->map[(int)(y / MMAP_SCALE)][(int)(x / MMAP_SCALE)].type == ' '
+		|| x < 0 || y < 0 || x > world->graphics.mmap_w
+		|| y > world->graphics.mmap_h)
+		return ;
+	world->player.mx = x;
+	world->player.my = y;
 	render(world);
 }
